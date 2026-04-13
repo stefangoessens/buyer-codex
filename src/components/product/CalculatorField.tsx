3b9a002f6d54548d44a1849ce1a30d7829a4c30a
stefangoessens <goessens.stefan@gmail.com>
2026-04-13 17:10:44 -0400

    KIN-945 fix shared component test runtime

--- src/components/product/CalculatorField.tsx
+++ src/components/product/CalculatorField.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface CalculatorFieldProps {
   id: string;
   label: string;
--- src/components/product/PropertyCard.tsx
+++ src/components/product/PropertyCard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
 import { ScoreBadge } from "./ScoreBadge";
